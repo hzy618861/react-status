fc39516f7a5ceeb986602dc3094b1036d95db01e
15172751061 <15172751061@163.com>
2022-01-28 14:56:14 +0800

    1

--- react-redux/src/store/sagas/counter.saga.js
+++ react-redux/src/store/sagas/counter.saga.js
@@ -1,7 +1,7 @@
 import {takeEvery,put, delay} from 'redux-saga/effects'
 import { increment } from '../actions/counter.action'
-import { INCREMNET_ASYNC } from '../const/counter.const'
-
+// import { INCREMNET_ASYNC } from '../const/counter.const'
+import {increment_async} from '../actions/counter.action'
 //takeEvery接受action
 //put触发action
 function* increment_async_fn(action){
@@ -10,5 +10,6 @@ function* increment_async_fn(action){
 }
 
 export default function* counterSaga(){
-     yield takeEvery(INCREMNET_ASYNC,increment_async_fn) 
+    //  yield takeEvery(INCREMNET_ASYNC,increment_async_fn) 
+     yield takeEvery(increment_async,increment_async_fn) 
 }
