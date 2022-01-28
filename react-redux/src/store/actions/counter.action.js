29c59568ced0231d78930150b510f948beca82a0
15172751061 <15172751061@163.com>
2022-01-28 14:52:25 +0800

    1

--- react-redux/src/store/actions/counter.action.js
+++ react-redux/src/store/actions/counter.action.js
@@ -1,21 +1,26 @@
-import {DECREMNET,INCREMNET,INCREMNET_ASYNC,DECREMNET_ASYNC} from '../const/counter.const'
-export const increment = payload => ({
-    type:INCREMNET,
-    payload
-})
-export const decrement = payload => ({
-    type:DECREMNET,
-    payload
-})
-//redux-saga方式
-export const increment_async = payload => ({
-    type:INCREMNET_ASYNC,
-    payload
-})
-export const decrement_async = payload => ({
-    type:DECREMNET_ASYNC,
-    payload
-})
+// import {DECREMNET,INCREMNET,INCREMNET_ASYNC,DECREMNET_ASYNC} from '../const/counter.const'
+import { createAction } from 'redux-actions'
+export const increment = createAction('increment')
+export const decrement = createAction('decrement')
+export const increment_async = createAction('increment_async')
+export const decrement_async = createAction('decrement_async')
+// export const increment = payload => ({
+//     type:INCREMNET,
+//     payload
+// })
+// export const decrement = payload => ({
+//     type:DECREMNET,
+//     payload
+// })
+// //redux-saga方式
+// export const increment_async = payload => ({
+//     type:INCREMNET_ASYNC,
+//     payload
+// })
+// export const decrement_async = payload => ({
+//     type:DECREMNET_ASYNC,
+//     payload
+// })
 //redux-thunk
 // export const increment_async = payload => dispatch => {
 //        setTimeout(()=>{
--- react-redux/src/store/reducers/counter.reducer.js
+++ react-redux/src/store/reducers/counter.reducer.js
@@ -1,22 +1,37 @@
-import {DECREMNET,INCREMNET} from '../const/counter.const'
+// import {DECREMNET,INCREMNET} from '../const/counter.const'
+import {handleActions as createReducer} from 'redux-actions'
+import {increment,decrement} from '../actions/counter.action'
 const initState = {
-    count: 0,
-
+    count: 0
   }
- export default function reducer(state=initState,action){
-     switch(action.type){
-          case INCREMNET:
-            return {
-              ...state,
-              count:state.count+action.payload
-           }
-          case DECREMNET:
-            return {
-              ...state,
-              count:state.count-action.payload
-          }
-          default:
-            return state
+  function handleIncrement(state,count){
+     return {
+       count:state.count+1
      }
   }
+  function handleDecrement(state,count){
+    return {
+      count:state.count-1
+    }
+ }
+  export default createReducer({
+       [increment]: handleIncrement,
+       [decrement]: handleDecrement,
+  },initState)
+//  export default function reducer(state=initState,action){
+//      switch(action.type){
+//           case INCREMNET:
+//             return {
+//               ...state,
+//               count:state.count+action.payload
+//            }
+//           case DECREMNET:
+//             return {
+//               ...state,
+//               count:state.count-action.payload
+//           }
+//           default:
+//             return state
+//      }
+//   }
   
