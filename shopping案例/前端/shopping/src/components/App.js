61d6a72918de55060523e7431be79c6d5ed1b8f8
15172751061 <15172751061@163.com>
2022-01-28 15:30:40 +0800

    1

---
+++ shopping案例/前端/shopping/src/components/App.js
@@ -0,0 +1,12 @@
+import Product from './product'
+import Cart from './cart'
+function App() {
+  return (
+    <div className="App">
+        <Cart/>
+        <Product/>
+    </div>
+  );
+}
+
+export default App;
